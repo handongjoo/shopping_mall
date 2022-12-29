5ae8b23c1a57e3bde5c64fb3f9ce32eac3fe91a9
handongjoo <dongjooyoung@naver.com>
2022-12-29 15:00:12 +0900

    로그인, 회원가입

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const User = require('./models/user.js');
+
+mongoose.connect("mongodb://127.0.0.1:27017/spa_mall", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+
+const app = express();
+const router = express.Router();
+
+// 회원가입 API
+router.post('/users', async (req, res) => {
+  try{
+    const {nickname, email, password, confirmPassword} = req.body;
+
+    // 1. 패스워드가 일치하는가
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        errorMessage: "패스워드가 일치하지 않습니다."
+      });
+    }
+
+    // 2. email 및 nickname에 해당하는 사용자가 있는가?
+
+    const existUser = await User.findOne({
+      $or: [{email: email}, {nickname: nickname}]
+    });
+
+    if (existUser) {
+      return res.status(400).json({
+        errorMessage: "중복된 email 혹은 닉네임입니다."
+      });
+    };
+    // 3. DB에 데이터 삽입
+
+    return await User.create({nickname, email, password}),
+    res.status(201).json({});
+    
+  } catch(error) {
+
+    console.error(error);
+
+    res.status(500).json({message: error.Message})
+  }
+  
+});
+
+router.post('/auth', async (req, res) =>{
+  try{
+    const {email, password} = req.body;
+
+    const user = await User.findOne({email});
+  if (!user || password !== user.password) {
+    return res.status(400).json({
+      errorMessage: "사용자가 존재하지 않거나 비밀번호가 틀렸습니다."
+    })
+  }
+  const token = jwt.sign({userId:user._id}, "sparta-secret-key")
+
+  return res.status(200).json({
+    "token": token
+  })
+
+  } catch(error) {
+
+    console.error(error);
+
+    res.status(500).json({message: error.Message})
+  }
+  
+})
+
+const authMiddleware = require("./middlewares/auth-middleware.js");
+router.get('/users/me', authMiddleware, async (req, res) => {
+  res.send({user: res.locals.user});
+});
+
+app.use("/api", express.urlencoded({ extended: false }), router);
+app.use(express.static("assets"));
+
+app.listen(8080, () => {
+  console.log("서버가 요청을 받을 준비가 됐어요");
+});
---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,39 @@
+// 미들웨어를 사용하기 위해 필요한것
+// 1. jwt
+// 2. 모델
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.js");
+
+module.exports = async (req, res, next) => {
+    try{
+        // jwt 가져오기
+    const {authorization} = req.headers;
+    // 검증
+    console.log(authorization);
+    // 분리
+    const [authType, authToken] = authorization.split(" ");
+    // authType : Bearer
+    // authToken : 실제 jwt 값
+    console.log([authType, authToken]);
+
+    if (authType !== "Bearer" || !authToken) {
+        res.status(400).json({
+            errorMessage: "로그인 후 사용 가능합니다."
+        });
+    }
+
+    //jwt가 유효한가 복호화 및 검증
+    const {userId} = jwt.verify(authToken, "sparta-secret-key");
+    const user = await User.findById(userId);
+    // res.locals.user : DB에 가지 않고도 변수로 따로 저장해 둬서 데이터 가져옴
+    res.locals.user = user;
+    console.log(user);
+    // 미들웨어를 다음으로 넘김
+    next();
+
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({message: error.message});
+    };
+    
+};
